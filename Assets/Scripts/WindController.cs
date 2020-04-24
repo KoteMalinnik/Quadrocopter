c9987020ceb9ae741cdeaeae4a93017ca896a439
Nikita Kalinnikov <niki9888985@mail.ru>
2020-04-25 01:34:19 +0300

    сделает ветер с любой стороны

--- Assets/Scripts/WindController.cs
+++ Assets/Scripts/WindController.cs
@@ -11,16 +11,25 @@ public class WindController : MonoBehaviour
 
 	static bool isOn = false;
 
+
+	static Vector3 windDirection = Vector3.zero;
 	public static void toggleWind(bool newIsOn)
 	{
 		isOn = newIsOn;
+
+		if (!isOn) return; //Если ветер выключили, то код ниже не выполнится 
+
+		windDirection = Vector3.one * 5;
+		windDirection.x *= Random.Range(0.1f, 0.7f);
+		windDirection.y *= Random.Range(0.1f, 0.7f);
+		windDirection.z *= Random.Range(0.1f, 0.7f);
 	}
 
     void FixedUpdate()
     {
 		if(isOn)
 		{
-			target.AddRelativeForce(Vector3.forward*windForce, ForceMode.Force);	
+			target.AddRelativeForce(windDirection*windForce, ForceMode.Force);	
 		}
     }
 }
